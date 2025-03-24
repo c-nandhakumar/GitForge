822027843aaa334dd1c5632fbad584663e831597
c-nandhakumar <nandha2402@gmail.com>
2025-03-24 16:17:24 +0530

    Implemented Reading a blob object

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import java.util.zip.InflaterInputStream
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
@@ -10,18 +11,38 @@ fun main(args: Array<String>) {
         exitProcess(1)
     }
 
-    if (args[0] == "init") {
-        // Uncomment this block to pass the first stage
-        //
-         val gitDir = File(".git")
-         gitDir.mkdir()
-         File(gitDir, "objects").mkdir()
-         File(gitDir, "refs").mkdir()
-         File(gitDir, "HEAD").writeText("ref: refs/heads/master\n")
-        
-         println("Initialized git directory")
-    } else {
-        println("Unknown command: ${args[0]}")
-        exitProcess(1)
+
+    when (args[0]){
+        "init" -> gitInit()
+        "cat-file" -> catFile(args)
+        else -> {
+            println("Unknown command: ${args[0]}")
+            exitProcess(1)
+        }
+    }
+}
+
+
+fun gitInit(){
+    val gitDir = File(".git")
+    gitDir.mkdir()
+    File(gitDir, "objects").mkdir()
+    File(gitDir, "refs").mkdir()
+    File(gitDir, "HEAD").writeText("ref: refs/heads/master\n")
+
+    println("Initialized git directory")
+}
+
+fun catFile(args: Array<String>) {
+    when(args[1]) {
+        "-p" ->{
+            val blob = File(".git/objects/${args[2].take(2)}/${args[2].drop(2)}")
+            val output = InflaterInputStream(blob.inputStream()).reader().readText()
+            .substringAfter("0".toByte().toString(), "")
+            print(output)
+        }
+        else -> {
+            print("Unimplemented : ${args[1]}")
+        }
     }
 }
