51fbbd3669f7a2b2573aaa5e96032c1beb29c456
c-nandhakumar <nandha2402@gmail.com>
2025-03-24 18:18:26 +0530

    Create a blob object

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 import java.io.File
+import java.security.MessageDigest
+import java.util.zip.DeflaterOutputStream
 import java.util.zip.InflaterInputStream
 import kotlin.system.exitProcess
 
@@ -15,6 +17,7 @@ fun main(args: Array<String>) {
     when (args[0]){
         "init" -> gitInit()
         "cat-file" -> catFile(args)
+        "hash-object" -> hashObject(args)
         else -> {
             println("Unknown command: ${args[0]}")
             exitProcess(1)
@@ -45,4 +48,30 @@ fun catFile(args: Array<String>) {
             print("Unimplemented : ${args[1]}")
         }
     }
+}
+
+fun hashObject(args: Array<String>){
+    when(args[1]){
+        "-w" -> {
+            val file = File(args[2])
+            val content = file.readBytes()
+            val header = "blob ${content.size}\u0000"
+            val blobContent = "$header${content.toString(Charsets.UTF_8)}"
+            val hashedContent = blobContent.hash("SHA-1")
+            println(hashedContent)
+
+            val outputFile = File(".git/objects/${hashedContent.take(2)}/${hashedContent.drop(2)}")
+            outputFile.parentFile.mkdirs()
+            DeflaterOutputStream(outputFile.outputStream()).use {
+                it.write(blobContent.toByteArray())
+            }
+        } else -> {
+           print("Unimplemented : ${args[1]}")
+        }
+    }
+}
+
+@OptIn(ExperimentalStdlibApi::class)
+private fun String.hash(algorithm: String): String {
+    return MessageDigest.getInstance(algorithm).digest(this.toByteArray()).toHexString()
 }
